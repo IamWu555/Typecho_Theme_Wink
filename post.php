b8e9687b2a481712cbbc9b904db164e13870deb0
Wu1638001908 <43913141+Wu1638001908@users.noreply.github.com>
2019-10-20 11:47:02 +0800

    Add files via upload

--- post.php
+++ post.php
@@ -1,12 +1,21 @@
 <?php $this -> need('header.php'); ?>
 <div class="main">
   <div class="main-content">
-    <div class="post-header" style="background-image:url(<?php  $this->fields->imgurl(); ?>);">
-      <div class="post-header-info">
-        <h2><?php $this->title();?></h2>
-        <span><?php $this->date('F j, Y'); ?> · <?php $this->category(' · '); ?></span>
+    <?php if (array_key_exists('imgurl',unserialize($this->___fields()))): ?>
+      <div class="post-header" style="background-image:url(<?php  $this->fields->imgurl(); ?>);">
+        <div class="post-header-info">
+          <h2><?php $this->title();?></h2>
+          <span><?php $this->date('F j, Y'); ?> · <?php $this->category(' · '); ?></span>
+        </div>
       </div>
-    </div>
+    <?php else : ?>
+      <div class="post-header" style="background-image:url(https://pic.superbed.cn/item/5d5f9dc4451253d178226678.jpg);">
+        <div class="post-header-info">
+          <h2><?php $this->title();?></h2>
+          <span><?php $this->date('F j, Y'); ?> · <?php $this->category(' · '); ?></span>
+        </div>
+      </div>
+    <?php endif; ?>
     <div class="post-content">
        <?php $this->content(); ?>
     </div>
